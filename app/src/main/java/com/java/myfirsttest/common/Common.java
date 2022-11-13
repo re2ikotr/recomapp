e080825a2ba54e313dae0b8e20875f0736836f8a
re2ikotr <re3ikotr@gmail.com>
2022-11-14 01:46:26 +0800

    init a floating window demo

---
+++ app/src/main/java/com/java/myfirsttest/common/Common.java
@@ -0,0 +1,14 @@
+package com.java.myfirsttest.common;
+
+public class Common {
+    // The EditText String will be
+    // stored in this variable
+    // when MINIMIZE or MAXIMIZE
+    // button is pressed
+    public static String currentDesc = "";
+
+    // The EditText String will be
+    // stored in this variable
+    // when SAVE button is pressed
+    public static String savedDesc = "";
+}
