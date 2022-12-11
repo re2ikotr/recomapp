e61f0fbaba261b0640845afea0f2b10fd755fdc1
re2ikotr <re3ikotr@gmail.com>
2022-12-11 17:54:54 +0800

    fix white list bug

--- app/src/main/java/com/java/recomapp/whitelist/WhiteListEntry.java
+++ app/src/main/java/com/java/recomapp/whitelist/WhiteListEntry.java
@@ -3,7 +3,7 @@ package com.java.recomapp.whitelist;
 public class WhiteListEntry {
     private String package_name;
     private String app_name;
-    private Boolean is_in_whitelist;
+    public Boolean is_in_whitelist;
     public WhiteListEntry(String _package_name, Boolean _is_in_whitelist) {
         this.package_name = _package_name;
         this.is_in_whitelist = _is_in_whitelist;
@@ -13,7 +13,4 @@ public class WhiteListEntry {
         return package_name;
     }
 
-    public Boolean getIs_in_whitelist() {
-        return is_in_whitelist;
-    }
 }
